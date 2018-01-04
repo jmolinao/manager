5036b226e462ab1a2baacb2ca0a31597ad31c2c3
jmolinao <jorge.molina17@inacapmail.cl>
2018-01-04 10:39:41 -0300

    Tracking Action works fine
    - Finalmente, y solo para comprobar que todo va bien, agrego un console.log a mu reducer, haciendo que muestre el objeto action recibido.

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -3,6 +3,7 @@ import { EMAIL_CHANGE, PASSWORD_CHANGE } from '../actions/types';
 const INITIAL_STATE = { email: '', password: '' };
 
 export default (state = INITIAL_STATE, action) => {
+  console.log(action);
   switch (action.type) {
     case EMAIL_CHANGE:
       return { ...state, email: action.payload };
