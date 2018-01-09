a567bb3b31bf69ef9f4f1c7c7f697045d0983cb0
jmolinao <jorge.molina17@inacapmail.cl>
2018-01-09 17:43:33 -0300

    Replace LoginForm -> Router
    - Reemplazo en mi index file, toda la renderización del LoginForm por la renderización completa del Router.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 /* @flow */
 
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
-import { Header } from './components/common';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class Index extends Component {
 
@@ -26,13 +24,10 @@ class Index extends Component {
 
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-    
+
     return (
       <Provider store={ store }>
-        <View>
-          <Header headerText="MI APP"/>
-          <LoginForm />
-        </View>
+        <Router />
       </Provider>
     );
   }
